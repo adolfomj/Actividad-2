d4523c1d131e9d52fad8962d7946aaa9fe5ace94
adolfomj <adolfo.jimenezm@est.iudigital.edu.co>
2025-09-16 22:27:00 -0500

    Add files via upload

---
+++ sql/create_database.sql
@@ -0,0 +1,3 @@
+#Con este script creamos la base de datos y con "IF NOT EXISTS" comprobamos si no existe antes de crearla
+CREATE DATABASE IF NOT EXISTS proyectos_informaticos;
+USE proyectos_informaticos;
